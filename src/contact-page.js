0970070fb0f4ea1b5f471b5373c25d5edfaed606
trent <122744132+trentbenson08@users.noreply.github.com>
2023-05-13 19:37:41 -0500

    Add contact page Change styles

--- src/contact-page.js
+++ src/contact-page.js
@@ -1,9 +1,43 @@
-// const page = `
-// <div id="menu" class="menu">
-// Contact
-// </div>
-// `
+/* <div id="contact" class="cont">
+<div id="contact-container" class="cont">
+  <div class="cont-item cont"><span class="label cont">Phone:</span><span class="info cont">(123) 456-7890</span></div>
+  <div class="cont-item cont"><span class="label cont">Email:</span><span class="info cont">contact@cafeitalia.com</span></div>
+  <div class="cont-item cont"><span class="label cont">Address:</span><span class="info cont">1234 1st Ave, New York, NY 12345</span></div>
+</div>
+</div> */
+
+const contactDetails = [
+  {label: 'Phone', info: '(123) 456-7890'},
+  {label: 'Email', info:'contact@cafeitalia.com'},
+  {label: 'Address', info: '1234 1st Ave, New York, NY 12345'}
+]
 
 export default function render(doc){
+  const article = doc.querySelector('#article-container');
+
+  const contact = doc.createElement('div');
+  contact.id = 'contact';
+  contact.className = 'cont';
+
+  const container = doc.createElement('div');
+  container.id = 'contact-container';
+  container.className = 'cont';
   
+  contactDetails.forEach(detail => {
+    const contactItem = doc.createElement('div');
+    contactItem.className = 'cont-item cont';
+
+    const label = doc.createElement('div');
+    label.className = 'label cont';
+    label.innerText = `${detail.label  }:`;
+
+    const info = doc.createElement('div');
+    info.className = 'info cont';
+    info.innerText = detail.info;
+    contactItem.append(label, info);
+    container.append(contactItem);
+  })
+
+  contact.append(container);
+  article.append(contact);
 }
